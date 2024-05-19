7b16e02a1a7f0064c7c9150f62a04fa2f9aa5b6b
Bùi Hoàng Tú <bhtu.work@gmail.com>
2024-05-19 20:57:14 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="functionize-notebook",
-    version="0.0.2",
+    version="0.0.3",
     author="Bui Hoang Tu",
     author_email="bhtu.work@gmail.com",
     url="https://github.com/BuiHoangTu/functionize-notebook/tree/release",
